bd8ff799f2aa3792e901c73e2f112e6288e1e237
tejaroy12 <112643817+tejaroy12@users.noreply.github.com>
2025-04-07 16:53:41 +0530

    ProductCard.jsx

--- src/Home/ProductCard.jsx
+++ src/Home/ProductCard.jsx
@@ -10,7 +10,7 @@ const ProductCard = ({ product, expanded, toggleExpand, handleBuyNow }) => {
           <h3 className="product-name">{product.name_te}</h3>
           <p className="product-company">{product.company}</p>
         </div>
-        <p className="product-description">
+        {/* <p className="product-description">
           {expanded
             ? product.description
             : product.description.length > 100
@@ -21,24 +21,38 @@ const ProductCard = ({ product, expanded, toggleExpand, handleBuyNow }) => {
               {expanded ? "view less" : "view more"}
             </span>
           )}
-        </p>
+        </p> */}
         <button className="voice-button" onClick={() => speakTelugu(product.description)}>
           🔊 Description వినండి
         </button>
         <div className="price-buy">
           <p className="product-price">₹{product.price}</p>
+
+          {product.quantity > 0 ? 
           <button 
-            className="buy-button" 
-            onClick={(e) => {
-                e.stopPropagation();
-                {const sentence = `${product.name_te} కార్ట్ జోడించబడింది. మీరు కొనాలనుకుంటే టోటల్ బటన్ పై క్లిక్ చేయండి.`;
-                speakTelugu(sentence);}
-                // speakTelugu(`${product.name_te} కార్ట్ జోడించబడింది. మీరు కొనాలనుకుంటే టోటల్ బటన్ పై క్లిక్ చేయండి.`);
-                handleBuyNow(product);
-            }}
-            >
-            Buy Now
-            </button>
+          className="buy-button" 
+          onClick={(e) => {
+              e.stopPropagation();
+              {const sentence = `${product.name_te} మీ కార్ట్‌లో విజయవంతంగా. కొనుగోలు కొనసాగించాలంటే, దయచేసి దిగువనున్న టోటల్ బటన్‌పై క్లిక్ చేయండి.`;
+              speakTelugu(sentence);}
+              // speakTelugu(`${product.name_te} కార్ట్ జోడించబడింది. మీరు కొనాలనుకుంటే దిగువన టోటల్ బటన్ పై క్లిక్ చేయండి.`);
+              handleBuyNow(product);
+          }}
+          >
+          Buy Now
+          </button>
+          :<button
+          className="out-of-stock-button"
+          onClick={(e) => {
+            e.stopPropagation();
+            {const sentence = `ప్రస్తుతం ${product.name_te} ఉత్పత్తి అందుబాటులో లేదు.`;
+            speakTelugu(sentence);}
+            // speakTelugu(`${product.name_te} కార్ట్ జోడించబడింది. మీరు కొనాలనుకుంటే టోటల్ బటన్ పై క్లిక్ చేయండి.`);
+
+          }}
+          >
+            Out Of Stock
+            </button>}
 
         </div>
       </div>
