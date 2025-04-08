d0fec1839a25193cb20f08280989b994de0b0a3a
tejaroy12 <112643817+tejaroy12@users.noreply.github.com>
2025-04-08 22:38:43 +0530

    index.js

--- src/Header/index.js
+++ src/Header/index.js
@@ -18,8 +18,8 @@ const Header = () => {
 
       <nav className={`nav ${isOpen ? "open" : ""}`}>
         <a href="/" className="nav-link" onClick={() => setIsOpen(false)}>Home</a>
-        <a href="/about" className="nav-link" onClick={() => setIsOpen(false)}>About</a>
-        <a href="tel:919390315670" className="nav-link" onClick={() => setIsOpen(false)}>📞 ఫోన్ చెయ్యండి</a>
+        <a href="/about" className="nav-link" onClick={() => setIsOpen(false)}>మా గురించి</a>
+        <a href="tel:919390315670" className="nav-link" onClick={() => setIsOpen(false)}>📞సమాచారము కొరకు ఫోన్ చెయ్యండి</a>
       </nav>
     </header>
   );
