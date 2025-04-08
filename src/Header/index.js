6f762a97487616f8556357aa28bda7bfc6d976b0
tejaroy12 <112643817+tejaroy12@users.noreply.github.com>
2025-04-08 21:18:24 +0530

    index.js

--- src/Header/index.js
+++ src/Header/index.js
@@ -6,7 +6,7 @@ const Header = () => {
 
   return (
     <header className="header">
-      <div className="logo">🌾 Sri Lakshmi Narasimha</div>
+      <div className="logo">🌾 Green Mart</div>
 
       <button
         className="menu-toggle"
